e07ff66fc0287e151e28eba93c897922a5aca002
Roland Pheasant <roland_pheasant@hotmail.com>
2015-04-19 08:52:53 +0100

    Update to latest dynamic data

--- Source/DynamicData.ReactiveUI/ReactiveListEx.cs
+++ Source/DynamicData.ReactiveUI/ReactiveListEx.cs
@@ -3,7 +3,6 @@ using System.Collections.Specialized;
 using System.Linq;
 using System.Reactive.Disposables;
 using System.Reactive.Linq;
-using DynamicData.Kernel;
 using ReactiveUI;
 
 namespace DynamicData.ReactiveUI
--- Source/DynamicData.ReactiveUI/SortedReactiveListAdaptor.cs
+++ Source/DynamicData.ReactiveUI/SortedReactiveListAdaptor.cs
@@ -1,7 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using DynamicData.Operators;
 using ReactiveUI;
 
 namespace DynamicData.ReactiveUI
